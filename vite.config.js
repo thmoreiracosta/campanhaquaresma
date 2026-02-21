71706ae8900bfd9caf4c237d24cbccda8a540f66
Thiago Costa <thiagomoreiracosta@gmail.com>
2026-02-21 15:36:51 -0300

    deployer registro.br

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: "/campanhaquaresma/", // 👈 MUITO IMPORTANTE
+  base: "/", // 👈 MUITO IMPORTANTE
 });
