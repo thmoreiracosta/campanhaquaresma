5dff97362a179b0511c506c59738b97d4bb6fdcd
Thiago Costa <thiagomoreiracosta@gmail.com>
2026-02-22 16:27:11 -0300

    apoiadores

--- src/data/apoiadores.js
+++ src/data/apoiadores.js
@@ -1,6 +1,9 @@
 import logoACC from "../assets/logo-atelie-caritas-christi.png";
 import logoLepanto from "../assets/LOGO LEPANTO - BG Vermelho.png";
 import logoPF from "../assets/logo-progredirnafe.png";
+import logoNCS from "../assets/logo-nacomunhaodossantos.png";
+import logoECV from "../assets/logo-evangelizandocomvc.png";
+
 
 export const apoiadores = [
   {
@@ -20,5 +23,17 @@ export const apoiadores = [
     name: "Lepanto",
     image: logoLepanto,
     instagram: "https://www.instagram.com/grupodeestudoslepanto/",
+  },
+  {
+    id: 4,
+    name: "Na Comunhão dos Santos",
+    image: logoNCS,
+    instagram: "https://www.instagram.com/na_comunhao_dos_santos",
+  },
+  {
+    id: 5,
+    name: "Evangelizando com Você",
+    image: logoECV,
+    instagram: "https://www.instagram.com/evangelizandocomvoce",
   },
 ];
